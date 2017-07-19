1de578093a0d2ce21faf547f2dea227b11a3cd49
alazo <alain.zosso@webzos.com>
2017-07-19 12:27:30 +0200

    calc_imputations update

--- stages/models.py
+++ stages/models.py
@@ -1,7 +1,8 @@
-from datetime import date, timedelta
 import json
 
+from datetime import date, timedelta
 from django.db import models
+from collections import OrderedDict
 
 from . import utils
 
@@ -104,12 +105,12 @@ class Teacher(models.Model):
 
     def calc_imputations(self):
         """
-        Return a tupple for accountings charges
+        Return a tuple for accountings charges
         """
         activities = self.calc_activity()
         imputations = OrderedDict()
         courses = self.course_set.all()
-        
+
         l1 = ['ASA', 'ASSC', 'ASE', 'MP', 'EDEpe', 'EDEps', 'EDS', 'CAS-FPP', 'Direction']
         for k in l1:
             imputations[k] = courses.filter(imputation__contains=k).aggregate(models.Sum('period'))['period__sum'] or 0
@@ -118,11 +119,9 @@ class Teacher(models.Model):
         if tot > 0:
             for k in l1:
                 imputations[k] += round(imputations[k] / tot * activities['tot_formation'])
-            
-        """
-        Split EDE périods in EDEpe and EDEps columns, in proportion
-        """
-        ede = courses.filter(imputation = 'EDE').aggregate(models.Sum('period'))['period__sum'] or 0
+
+        #Split EDE périods in EDEpe and EDEps columns, in proportion
+        ede = courses.filter(imputation='EDE').aggregate(models.Sum('period'))['period__sum'] or 0
         if ede > 0:
             pe = imputations['EDEpe']
             ps = imputations['EDEps']
@@ -130,10 +129,10 @@ class Teacher(models.Model):
             pe_plus = pe * pe_percent
             imputations['EDEpe'] += pe_plus
             imputations['EDEps'] += ede-pe_plus
-                   
+
         return (self.calc_activity(), imputations)
-    
-    
+
+
 class Student(models.Model):
     ext_id = models.IntegerField(null=True, unique=True, verbose_name='ID externe')
     first_name = models.CharField(max_length=40, verbose_name='Prénom')
@@ -403,6 +402,7 @@ class Course(models.Model):
         verbose_name_plural = 'Cours'
 
     def __str__(self):
-        return '{0} - {1} - {2} - {3} - {4}'.format(
-            self.teacher, self.klass, self.subject, self.period, self.section
+        return '{0} - {1} - {2} - {3}'.format(
+            self.teacher, self.public, self.subject, self.period
         )
+
